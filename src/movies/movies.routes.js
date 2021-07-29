6f13e52c0770b821ec67e72e64982ca6edc63f5d
hodan97 <83996550+hodan97@users.noreply.github.com>
2021-07-29 18:09:58 +0100

    error

--- src/movies/movies.routes.js
+++ src/movies/movies.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const movieRouter = Router();
-const {addMovie, findMovie}= require('./movies.controllers');
+const {addMovie, findMovie, deleteMovie, updateMovie}= require('./movies.controllers');
 
 movieRouter.post('/movies', addMovie);
 movieRouter.get('/movies/:title', findMovie);
+movieRouter.delete('/movies/:title', deleteMovie);
+movieRouter.put('/movies/:title', updateMovie);
 
 module.exports = movieRouter;
